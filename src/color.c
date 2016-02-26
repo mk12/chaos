7d50cb071fea5ef1e56dea78e2e9e453fd35c2ed
Mitchell Kember <mk12360@gmail.com>
2016-02-25 20:46:45 -0800

    Greyscale!

--- src/color.c
+++ src/color.c
@@ -2,14 +2,11 @@
 
 #include "color.h"
 
+#include <math.h>
+
 void alpha(FILE *out, double v) {
-	if (v < 0.5) {
-		putc(0, out);
-		putc(0, out);
-		putc(0, out);
-	} else {
-		putc(255, out);
-		putc(255, out);
-		putc(255, out);
-	}
+	unsigned char g = (unsigned char)(v * 255 + 0.5);
+	putc(g, out);
+	putc(g, out);
+	putc(g, out);
 }
